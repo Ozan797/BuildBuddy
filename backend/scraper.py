fb7f4a15527ea77e258f0d175d6dd09d7a054cb4
Ozan Gokberk <ozan8@hotmail.co.uk>
2023-12-11 11:52:09 +0000

    CPU and GPU info done. Need to add some more info for CPU

--- backend/scraper.py
+++ backend/scraper.py
@@ -1,5 +1,7 @@
 from bs4 import BeautifulSoup
+import re
 
+# CPU Web Scraper
 def scrape_cpu_info(file_path):
     return _scrape_cpu_info_from_html(file_path)
 
@@ -33,6 +35,8 @@ def _scrape_cpu_info_from_html(file_path):
 
     return cpu_info_list
 
+
+# GPU Web Scraper
 def scrape_gpu_info(file_path):
     return _scrape_gpu_info_from_html(file_path)
 
@@ -42,15 +46,41 @@ def _scrape_gpu_info_from_html(file_path):
         
     soup = BeautifulSoup(html_content, "html.parser")
     
-    gpu_names = soup.find_all('h3', class_='ProductNameTable-sc-1stvbsu-3 bbvppQ')
+    gpu_items = soup.find_all('tr', class_='Tr-sc-1stvbsu-2 chMRiA')[:60]  # Limit to a length of 60
+    gpu_info_list = [] # Initialise empty array to store info
     
-    gpu_info_list = []
-    
-    for name in gpu_names:
-        gpu_info_list.append({
-            'name': name.get_text(strip=True)
-        })
+    # For loop to parse through HTML Content
+    for item in gpu_items:
+        full_name = item.find('h3', class_='ProductNameTable-sc-1stvbsu-3 bbvppQ').get_text(strip=True)
+        price = item.find("span", class_="PriceLabel-sc-lboeq9-0").get_text(strip=True)
+        extracted_price = re.search(r'£[\d,.]+', price) # Removes any string excpet prices
+        
+        # Extracting GPU memory
+        memory = ''
+        gpu_properties = item.find_all('div', class_='PropertyContainer-sc-1stvbsu-11 TVmeo')
+        for prop in gpu_properties:
+            if 'gb' in prop.get_text(strip=True).lower(): # If "gb" is in string
+                memory = prop.get_text(strip=True) # Save to memory variable
+                break # Break loop
+        
+        if extracted_price:
+            brand = '' # Empty brand string
+            name = full_name  # Store the full name initially
+            
+            if 'geforce' in full_name.lower() or 'radeon' in full_name.lower() or "intel" in full_name.lower():
+                # Extracting brand from the GPU name
+                if 'geforce' in full_name.lower():
+                    brand = 'NVIDIA GeForce'
+                elif 'radeon' in full_name.lower():
+                    brand = 'AMD Radeon'
+                elif 'intel' in full_name.lower():
+                    brand = 'Intel ARC'
+                
+            gpu_info_list.append({
+                'name': full_name,  # Full name including brand
+                'brand': brand,
+                'memory': memory,
+                'price': extracted_price.group()
+            })
     
     return gpu_info_list
-    
-
