f1e192c14e405bb373633e2fb5a3932f9506584d
Ozan Gokberk <ozan8@hotmail.co.uk>
2023-12-11 23:19:11 +0000

    scraper works as intended and requests also work as intended

--- backend/main.py
+++ backend/main.py
@@ -1,14 +1,13 @@
-# Flask Server with Threading
 from flask import Flask, jsonify
 import concurrent.futures
 from scraper import scrape_cpu_info_from_urls, scrape_gpu_info_from_urls
 
 app = Flask(__name__)
 
+# Function to scrape data using threading
 def scrape_with_threading(urls, scraper_function):
     with concurrent.futures.ThreadPoolExecutor() as executor:
-        # Scrape data concurrently using threads
-        futures = {executor.submit(scraper_function, [url]): url for url in urls}  # Pass the URL as a list
+        futures = {executor.submit(scraper_function, [url]): url for url in urls}
         data = []
         for future in concurrent.futures.as_completed(futures):
             url = futures[future]
@@ -19,31 +18,26 @@ def scrape_with_threading(urls, scraper_function):
                 print(f"Failed to scrape data from {url}. Exception: {e}")
     return data
 
+# URLs to scrape CPU info
 
-# URL to scrape CPU info
-cpu_urls_to_scrape = [
-    "https://pricespy.co.uk/computers-accessories/computer-components/cpus--c500",
-    "https://pricespy.co.uk/computers-accessories/computer-components/cpus--c500?offset=24",
-    "https://pricespy.co.uk/computers-accessories/computer-components/cpus--c500?offset=48",
-]
-
-@app.route('/cpu_info', methods=['GET'])  # GET route for CPU Info
+@app.route('/cpu_info', methods=['GET'])  # Route to get CPU info
 def get_cpu_info():
-    print("CPU URLs to scrape:", cpu_urls_to_scrape)  # Print or log the CPU URLs
-
-    cpu_info = scrape_with_threading(cpu_urls_to_scrape, scrape_cpu_info_from_urls)
+    cpu_urls_to_scrape = [
+        "https://pricespy.co.uk/computers-accessories/computer-components/cpus--c500",
+        "https://pricespy.co.uk/computers-accessories/computer-components/cpus--c500?offset=24",
+        "https://pricespy.co.uk/computers-accessories/computer-components/cpus--c500?offset=48",
+        
+        ]   
+    cpu_info = scrape_cpu_info_from_urls(cpu_urls_to_scrape)
     return jsonify({'cpu_info': cpu_info})
 
-# GPU's
-gpu_urls_to_scrape = [
-    "https://pricespy.co.uk/c/graphics-cards",
-    "https://pricespy.co.uk/c/graphics-cards?offset=44",
-]
-
-@app.route("/gpu_info", methods=["GET"])
+@app.route("/gpu_info", methods=["GET"])  # Route to get GPU info
 def get_gpu_info():
-    print("GPU URLs to scrape:", gpu_urls_to_scrape)  # Print or log the GPU URLs
-    gpu_info = scrape_with_threading(gpu_urls_to_scrape, scrape_gpu_info_from_urls)
+    gpu_urls_to_scrape = [
+        "https://pricespy.co.uk/c/graphics-cards",
+        "https://pricespy.co.uk/c/graphics-cards?offset=44",
+    ]
+    gpu_info = scrape_gpu_info_from_urls(gpu_urls_to_scrape)
     return jsonify({"gpu_info": gpu_info})
 
 if __name__ == '__main__':
--- backend/scraper.py
+++ backend/scraper.py
@@ -1,61 +1,88 @@
 from bs4 import BeautifulSoup
 import re
 import requests
+import json
 
 # CPU Web Scraper
 def scrape_cpu_info_from_urls(urls):
-    cpu_info_list = []
+    file_path = 'cpu_info.json'
+    cpu_info_list = []  # Initialize list to store CPU information
 
+    # Check if the file already exists and load data if it does
+    if os.path.exists(file_path):
+        with open(file_path, 'r') as json_file:
+            cpu_info_list = json.load(json_file)
+
+    # Loop through each URL to scrape CPU info
     for url in urls:
-        print("Scraping URL:", url)
-
+        # Get HTML content from the URL
         response = requests.get(url)
-        if response.status_code == 200:
+        if response.status_code == 200:  # Check if the request was successful
             html_content = response.text
-            soup = BeautifulSoup(html_content, 'html.parser')
+            soup = BeautifulSoup(html_content, 'html.parser')  # Create BeautifulSoup object
 
-            # Find CPU names and prices
-            cpu_names = soup.find_all(class_='ProductName-sc-d3c4p4-6')[:24]
-            cpu_prices = soup.find_all('span', class_='PriceLabel-sc-lboeq9-0')[:24]
+            # Find CPU names and prices from the HTML content
+            cpu_names = soup.find_all(class_='ProductName-sc-d3c4p4-6')[:24]  # Limit to 24 CPU names
+            cpu_prices = soup.find_all('span', class_='PriceLabel-sc-lboeq9-0')[:24]  # Limit to 24 CPU prices
 
             # Extract relevant data for each CPU
             for name, price in zip(cpu_names, cpu_prices):
-                name = name.get_text(strip=True)
-                brand = name.split()[0] if name else ''  # Extracting the first word
+                name = name.get_text(strip=True)  # Extract CPU name
+                brand = name.split()[0] if name else ''  # Extracting the first word as brand
 
-                # Check if the brand is AMD or Intel
+                # Check if the brand is AMD or Intel and add to the CPU info list
                 if brand.lower() in ('amd', 'intel'):
-                    cpu_info_list.append({
-                        'name': name,
-                        'brand': brand,
-                        'price': price.get_text(strip=True)
-                    })
+                    # Check for duplicates before adding to the list
+                    cpu_exists = any(cpu['name'] == name for cpu in cpu_info_list)
+                    if not cpu_exists:
+                        cpu_info_list.append({
+                            'name': name,
+                            'brand': brand,
+                            'price': price.get_text(strip=True)
+                        })
         else:
+            # Print error message if fetching content fails
             print(f"Failed to fetch content from {url}. Status code: {response.status_code}")
 
-    return cpu_info_list
+    # Save the combined cpu_info_list to a JSON file after the loop
+    with open(file_path, 'w') as json_file:
+        json.dump(cpu_info_list, json_file, indent=4)
+
+    return cpu_info_list  # Return the list of CPU info
 
 
-# GPU Web Scraper
+import json
+import os.path
+import requests
+from bs4 import BeautifulSoup
+import re
+
 def scrape_gpu_info_from_urls(urls):
-    gpu_info_list = []  # Initialize empty array to store info
+    file_path = 'gpu_info.json'
+    gpu_info_list = []  # Initialize list to store GPU information
 
+    # Check if the file already exists and load data if it does
+    if os.path.exists(file_path):
+        with open(file_path, 'r') as json_file:
+            gpu_info_list = json.load(json_file)
+
+    # Loop through each URL to scrape GPU info
     for url in urls:
-        print("Scraping URL:", url)
-
+        # Get HTML content from the URL
         response = requests.get(url)
-        if response.status_code == 200:
+        if response.status_code == 200:  # Check if the request was successful
             html_content = response.text
-            soup = BeautifulSoup(html_content, "html.parser")
+            soup = BeautifulSoup(html_content, "html.parser")  # Create BeautifulSoup object
 
-            # Find GPU items
-            gpu_items = soup.find_all('tr', class_='Tr-sc-1stvbsu-2 chMRiA')[:60]  # Limit to a length of 60
+            # Find GPU items from the HTML content
+            gpu_items = soup.find_all('tr', class_='Tr-sc-1stvbsu-2 chMRiA')[:60]  # Limit to 60 GPU items
 
-            # Extract relevant data for each GPU
+            # Extract relevant data for each GPU item
             for item in gpu_items:
+                # Extract information
                 full_name = item.find('h3', class_='ProductNameTable-sc-1stvbsu-3 bbvppQ').get_text(strip=True)
                 price = item.find("span", class_="PriceLabel-sc-lboeq9-0").get_text(strip=True)
-                extracted_price = re.search(r'£[\d,.]+', price)  # Removes any string except prices
+                extracted_price = re.search(r'£[\d,.]+', price)  # Remove any string except prices
 
                 memory = ''
                 gpu_properties = item.find_all('div', class_='PropertyContainer-sc-1stvbsu-11 TVmeo')
@@ -66,9 +93,6 @@ def scrape_gpu_info_from_urls(urls):
 
                 if extracted_price:
                     brand = ''
-                    name = full_name
-
-                    # Extract GPU brand
                     if 'geforce' in full_name.lower() or 'radeon' in full_name.lower() or "intel" in full_name.lower():
                         if 'geforce' in full_name.lower():
                             brand = 'NVIDIA GeForce'
@@ -77,13 +101,18 @@ def scrape_gpu_info_from_urls(urls):
                         elif 'intel' in full_name.lower():
                             brand = 'Intel ARC'
 
-                    gpu_info_list.append({
-                        'name': full_name,
-                        'brand': brand,
-                        'memory': memory,
-                        'price': extracted_price.group()
-                    })
-        else:
-            print(f"Failed to fetch content from {url}. Status code: {response.status_code}")
+                    # Check for duplicates before adding to the list
+                    gpu_exists = any(gpu['name'] == full_name for gpu in gpu_info_list)
+                    if not gpu_exists:
+                        gpu_info_list.append({
+                            'name': full_name,
+                            'brand': brand,
+                            'memory': memory,
+                            'price': extracted_price.group()
+                        })
 
-    return gpu_info_list
+    # Save the combined gpu_info_list to a JSON file after the loop
+    with open(file_path, 'w') as json_file:
+        json.dump(gpu_info_list, json_file, indent=4)
+
+    return gpu_info_list  # Return the list of GPU info
