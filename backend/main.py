e1227df20d406759cad8aa3874c734b3231f338e
Ozan Gokberk <ozan8@hotmail.co.uk>
2023-12-11 16:16:29 +0000

    created scraper for URLS

--- backend/main.py
+++ backend/main.py
@@ -1,24 +1,49 @@
-# Flask Server
+# Flask Server with Threading
 from flask import Flask, jsonify
-from scraper import scrape_cpu_info_from_url, scrape_gpu_info
+import concurrent.futures
+from scraper import scrape_cpu_info_from_urls, scrape_gpu_info_from_urls
 
 app = Flask(__name__)
 
+def scrape_with_threading(urls, scraper_function):
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        # Scrape data concurrently using threads
+        futures = {executor.submit(scraper_function, [url]): url for url in urls}  # Pass the URL as a list
+        data = []
+        for future in concurrent.futures.as_completed(futures):
+            url = futures[future]
+            try:
+                result = future.result()
+                data.extend(result)
+            except Exception as e:
+                print(f"Failed to scrape data from {url}. Exception: {e}")
+    return data
+
+
 # URL to scrape CPU info
-CPU_INFO_URL = 'https://pricespy.co.uk/computers-accessories/computer-components/cpus--c500'
+cpu_urls_to_scrape = [
+    "https://pricespy.co.uk/computers-accessories/computer-components/cpus--c500",
+    "https://pricespy.co.uk/computers-accessories/computer-components/cpus--c500?offset=24",
+    "https://pricespy.co.uk/computers-accessories/computer-components/cpus--c500?offset=48",
+]
 
 @app.route('/cpu_info', methods=['GET'])  # GET route for CPU Info
 def get_cpu_info():
-    cpu_info = scrape_cpu_info_from_url(CPU_INFO_URL)  # Scrape CPU info from URL
-    
+    print("CPU URLs to scrape:", cpu_urls_to_scrape)  # Print or log the CPU URLs
+
+    cpu_info = scrape_with_threading(cpu_urls_to_scrape, scrape_cpu_info_from_urls)
     return jsonify({'cpu_info': cpu_info})
+
 # GPU's
-gpu_info_pg1_path = "gpu_info_pg1.html"
+gpu_urls_to_scrape = [
+    "https://pricespy.co.uk/c/graphics-cards",
+    "https://pricespy.co.uk/c/graphics-cards?offset=44",
+]
 
 @app.route("/gpu_info", methods=["GET"])
 def get_gpu_info():
-    gpu_info = scrape_gpu_info(gpu_info_pg1_path)
-    
+    print("GPU URLs to scrape:", gpu_urls_to_scrape)  # Print or log the GPU URLs
+    gpu_info = scrape_with_threading(gpu_urls_to_scrape, scrape_gpu_info_from_urls)
     return jsonify({"gpu_info": gpu_info})
 
 if __name__ == '__main__':
--- backend/scraper.py
+++ backend/scraper.py
@@ -3,78 +3,87 @@ import re
 import requests
 
 # CPU Web Scraper
-def scrape_cpu_info_from_url(url):
-    response = requests.get(url)
-    if response.status_code == 200:
-        html_content = response.text
-        soup = BeautifulSoup(html_content, 'html.parser')
+def scrape_cpu_info_from_urls(urls):
+    cpu_info_list = []
 
-        cpu_names = soup.find_all(class_='ProductName-sc-d3c4p4-6')[:24]
-        cpu_prices = soup.find_all('span', class_='PriceLabel-sc-lboeq9-0')[:24]
-        
-        cpu_info_list = []
-        for name, price in zip(cpu_names, cpu_prices):
-            name = name.get_text(strip=True)
-            brand = name.split()[0] if name else ''  # Extracting the first word
+    for url in urls:
+        print("Scraping URL:", url)
 
-            if brand.lower() in ('amd', 'intel'):  # Check if first word is AMD or Intel
-                cpu_info_list.append({
-                    'name': name,
-                    'brand': brand,
-                    'price': price.get_text(strip=True)
-                })
+        response = requests.get(url)
+        if response.status_code == 200:
+            html_content = response.text
+            soup = BeautifulSoup(html_content, 'html.parser')
 
-        return cpu_info_list
-    else:
-        print(f"Failed to fetch content from {url}. Status code: {response.status_code}")
-        return None
+            # Find CPU names and prices
+            cpu_names = soup.find_all(class_='ProductName-sc-d3c4p4-6')[:24]
+            cpu_prices = soup.find_all('span', class_='PriceLabel-sc-lboeq9-0')[:24]
+
+            # Extract relevant data for each CPU
+            for name, price in zip(cpu_names, cpu_prices):
+                name = name.get_text(strip=True)
+                brand = name.split()[0] if name else ''  # Extracting the first word
+
+                # Check if the brand is AMD or Intel
+                if brand.lower() in ('amd', 'intel'):
+                    cpu_info_list.append({
+                        'name': name,
+                        'brand': brand,
+                        'price': price.get_text(strip=True)
+                    })
+        else:
+            print(f"Failed to fetch content from {url}. Status code: {response.status_code}")
+
+    return cpu_info_list
 
 
 # GPU Web Scraper
-def scrape_gpu_info(file_path):
-    return _scrape_gpu_info_from_html(file_path)
+def scrape_gpu_info_from_urls(urls):
+    gpu_info_list = []  # Initialize empty array to store info
 
-def _scrape_gpu_info_from_html(file_path):
-    with open(file_path, "r", encoding="utf-8") as file:
-        html_content = file.read()
-        
-    soup = BeautifulSoup(html_content, "html.parser")
-    
-    gpu_items = soup.find_all('tr', class_='Tr-sc-1stvbsu-2 chMRiA')[:60]  # Limit to a length of 60
-    gpu_info_list = [] # Initialise empty array to store info
-    
-    # For loop to parse through HTML Content
-    for item in gpu_items:
-        full_name = item.find('h3', class_='ProductNameTable-sc-1stvbsu-3 bbvppQ').get_text(strip=True)
-        price = item.find("span", class_="PriceLabel-sc-lboeq9-0").get_text(strip=True)
-        extracted_price = re.search(r'£[\d,.]+', price) # Removes any string excpet prices
-        
-        # Extracting GPU memory
-        memory = ''
-        gpu_properties = item.find_all('div', class_='PropertyContainer-sc-1stvbsu-11 TVmeo')
-        for prop in gpu_properties:
-            if 'gb' in prop.get_text(strip=True).lower(): # If "gb" is in string
-                memory = prop.get_text(strip=True) # Save to memory variable
-                break # Break loop
-        
-        if extracted_price:
-            brand = '' # Empty brand string
-            name = full_name  # Store the full name initially
-            
-            if 'geforce' in full_name.lower() or 'radeon' in full_name.lower() or "intel" in full_name.lower():
-                # Extracting brand from the GPU name
-                if 'geforce' in full_name.lower():
-                    brand = 'NVIDIA GeForce'
-                elif 'radeon' in full_name.lower():
-                    brand = 'AMD Radeon'
-                elif 'intel' in full_name.lower():
-                    brand = 'Intel ARC'
-                
-            gpu_info_list.append({
-                'name': full_name,  # Full name including brand
-                'brand': brand,
-                'memory': memory,
-                'price': extracted_price.group()
-            })
-    
+    for url in urls:
+        print("Scraping URL:", url)
+
+        response = requests.get(url)
+        if response.status_code == 200:
+            html_content = response.text
+            soup = BeautifulSoup(html_content, "html.parser")
+
+            # Find GPU items
+            gpu_items = soup.find_all('tr', class_='Tr-sc-1stvbsu-2 chMRiA')[:60]  # Limit to a length of 60
+
+            # Extract relevant data for each GPU
+            for item in gpu_items:
+                full_name = item.find('h3', class_='ProductNameTable-sc-1stvbsu-3 bbvppQ').get_text(strip=True)
+                price = item.find("span", class_="PriceLabel-sc-lboeq9-0").get_text(strip=True)
+                extracted_price = re.search(r'£[\d,.]+', price)  # Removes any string except prices
+
+                memory = ''
+                gpu_properties = item.find_all('div', class_='PropertyContainer-sc-1stvbsu-11 TVmeo')
+                for prop in gpu_properties:
+                    if 'gb' in prop.get_text(strip=True).lower():
+                        memory = prop.get_text(strip=True)
+                        break
+
+                if extracted_price:
+                    brand = ''
+                    name = full_name
+
+                    # Extract GPU brand
+                    if 'geforce' in full_name.lower() or 'radeon' in full_name.lower() or "intel" in full_name.lower():
+                        if 'geforce' in full_name.lower():
+                            brand = 'NVIDIA GeForce'
+                        elif 'radeon' in full_name.lower():
+                            brand = 'AMD Radeon'
+                        elif 'intel' in full_name.lower():
+                            brand = 'Intel ARC'
+
+                    gpu_info_list.append({
+                        'name': full_name,
+                        'brand': brand,
+                        'memory': memory,
+                        'price': extracted_price.group()
+                    })
+        else:
+            print(f"Failed to fetch content from {url}. Status code: {response.status_code}")
+
     return gpu_info_list
