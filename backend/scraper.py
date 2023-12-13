424fdedd7f4f9aa112458f635c71315cf5e2bcae
Ozan Gokberk <ozan8@hotmail.co.uk>
2023-12-13 22:16:11 +0000

    fixed data types for CPU info and added extra fields for endpoint

--- backend/scraper.py
+++ backend/scraper.py
@@ -2,14 +2,13 @@ from bs4 import BeautifulSoup
 import re
 import requests
 
-# Filter CPU data by removing entries with empty fields
+# Function to filter out CPU entries with empty fields
 def filter_empty_cpu_fields(data):
     required_fields = ['name', 'brand', 'price']
-
     filtered_data = [entry for entry in data if all(entry.get(field) for field in required_fields)]
     return filtered_data
 
-# Scrape CPU info from the provided URL
+# Function to scrape CPU information from the given URL
 def scrape_cpu_info_from_url(url):
     cpu_info_list = []
 
@@ -18,34 +17,49 @@ def scrape_cpu_info_from_url(url):
         html_content = response.text
         soup = BeautifulSoup(html_content, 'html.parser')
 
+        # Extract CPU names and prices
         cpu_names = soup.find_all(class_='ProductName-sc-d3c4p4-6')[:24]
         cpu_prices = soup.find_all('span', class_='PriceLabel-sc-lboeq9-0')[:24]
 
         for name, price in zip(cpu_names, cpu_prices):
             name_text = name.get_text(strip=True)
-            brand = name_text.split()[0] if name_text else ''
+            brand = name_text.split()[0] if name_text else ''  # Extracting the brand from the CPU name
 
-            if brand.lower() in ('amd', 'intel'):
+            if brand.lower() in ('amd', 'intel'):  # Checking for AMD or Intel brands
+                price_value = float(price.get_text(strip=True)[1:].replace(',', ''))  # Convert price to float
+
+                # Extract GHz information from CPU name
+                frequency_text = re.search(r'(\d+\.\d+)GHz', name_text, re.IGNORECASE)  # Find GHz in the name
+
+                # Convert GHz string to MHz integer
+                if frequency_text:
+                    try:
+                        frequency = int(float(frequency_text.group(1)) * 1000)  # Convert GHz to MHz
+                    except ValueError:
+                        frequency = ''
+                else:
+                    frequency = ''
+
                 cpu_exists = any(cpu['name'] == name_text for cpu in cpu_info_list)
                 if not cpu_exists:
                     cpu_info_list.append({
                         'name': name_text,
                         'brand': brand,
-                        'price': price.get_text(strip=True)
+                        'price': price_value,
+                        'frequency': frequency
                     })
     else:
         print(f"Failed to fetch content from {url}. Status code: {response.status_code}")
 
     return filter_empty_cpu_fields(cpu_info_list)
 
-# Filter GPU data by removing entries with empty fields
+# Function to filter GPU entries with empty fields
 def filter_empty_gpu_fields(data):
     required_fields = ['name', 'brand', 'memory', 'price']
-
     filtered_data = [entry for entry in data if all(entry.get(field) for field in required_fields)]
     return filtered_data
 
-# Scrape GPU info from the provided URL
+# Function to scrape GPU information from the given URL
 def scrape_gpu_info_from_url(url):
     gpu_info_list = []
 
@@ -54,6 +68,7 @@ def scrape_gpu_info_from_url(url):
         html_content = response.text
         soup = BeautifulSoup(html_content, "html.parser")
 
+        # Find GPU items
         gpu_items = soup.find_all('tr', class_='Tr-sc-1stvbsu-2 chMRiA')[:60]
 
         for item in gpu_items:
@@ -93,13 +108,13 @@ def scrape_gpu_info_from_url(url):
 
     return filter_empty_gpu_fields(gpu_info_list)
 
-# Remove RAM entries with empty fields
+# Function to remove empty fields in RAM entries
 def filter_empty_ram_fields(data):
     required_fields = ['name', 'price', 'frequency', 'brand', 'ram_type']
     filtered_data = [entry for entry in data if all(entry.get(field) for field in required_fields)]
     return filtered_data
 
-# Scrape RAM information from a URL
+# Function to scrape RAM information from a URL
 def scrape_ram_info_from_url(url):
     ram_info_list = []
 
