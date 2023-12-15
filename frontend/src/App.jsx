de18c19345b3d16e8c00866c1b710f8d2bfedbd6
Ozan Gokberk <ozan8@hotmail.co.uk>
2023-12-15 14:28:33 +0000

    sent successful endpoint requests

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [responseData, setResponseData] = useState(null);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://127.0.0.1:5000/psu_info'); // Replace this URL with your API endpoint
+        setResponseData(response.data); // Assuming the response is JSON
+      } catch (error) {
+        // Handle error, for instance, set an error state
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
   return (
+    
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {responseData ? (
+        <pre>{JSON.stringify(responseData, null, 2)}</pre>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
     </>
   )
 }
