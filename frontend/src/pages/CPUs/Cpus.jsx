441c04ac434b028ac563dd8b6bfc5f5fba29701e
Ozan Gokberk <ozan8@hotmail.co.uk>
2023-12-16 22:07:20 +0000

    All pages have all data from api request

--- frontend/src/pages/CPUs/Cpus.jsx
+++ frontend/src/pages/CPUs/Cpus.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import Card from "../../components/Card/Card";
 
 const Cpus = () => {
-  return (
-    <div>Cpus</div>
-  )
-}
+  const [cpuData, setCpuData] = useState([]);
 
-export default Cpus
+  useEffect(() => {
+    fetchCPUData();
+  }, []);
+
+  const fetchCPUData = async () => {
+    try {
+      const response = await fetch("http://127.0.0.1:5000/cpu_info");
+      const cpuApiResponse = await response.json();
+      setCpuData(cpuApiResponse);
+    } catch (error) {
+      console.error("Error fetching CPU Data:", error);
+    }
+  };
+
+  return (
+    <>
+      <h1>CPU Page</h1>
+      <Card data={cpuData} />
+    </>
+  );
+};
+
+export default Cpus;
